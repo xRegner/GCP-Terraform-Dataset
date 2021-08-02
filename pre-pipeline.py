e525a22b30d5f031cd0e0589503afc8738aad1a3
ChristianBenitez <christianbenitez@ChristianBenitez-MacBook-Pro.local>
2021-08-02 18:41:41 -0500

    testing py+terraform

--- pre-pipeline.py
+++ pre-pipeline.py
@@ -1,10 +1,24 @@
+import json
 import mmap
 
-with open('example.txt') as f:
-    s = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-    if s.find('prevent_destroy') != -1:
-        newvar = s.__getattribute__
-        print(newvar)
-        print('true')
-    else: print('false')
+with open("my_file.json", "r") as f:
+    my_dict = json.load(f)
+    varDestoy = my_dict["prevent_destroy"]
+print(my_dict["prevent_destroy"])
 
+
+
+#read input file
+fin = open("datasets-sample-gcp-xtian/main.tf", "rt")
+#read file contents to string
+data = fin.read()
+#replace all occurrences of the required string
+data = data.replace('prevent_destroy = false', 'prevent_destroy = true')
+#close the input file
+fin.close()
+#open the input file in write mode
+fin = open("datasets-sample-gcp-xtian/main.tf", "wt")
+#overrite the input file with the resulting data
+fin.write(data)
+#close the file
+fin.close()
